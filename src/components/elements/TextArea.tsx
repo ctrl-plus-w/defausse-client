408b624bd4492f60b60cfb467ba2653d5c3b7e2e
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-22 17:39:57 +0100

    :truck: Added a TextArea element

--- src/components/elements/TextArea.tsx
+++ src/components/elements/TextArea.tsx
@@ -1,15 +1,19 @@
 import clsx from 'clsx';
+import { ChangeEventHandler } from 'react';
 
 interface IProps {
   name: string;
   label: string;
+
+  value?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement>;
 
   rows?: number;
 
   className?: string;
 }
 
-const TextArea = ({ name, label, rows, className }: IProps) => {
+const TextArea = ({ name, label, rows, value, onChange, className }: IProps) => {
   return (
     <label htmlFor={name} className={clsx(['flex flex-col gap-4', className])}>
       <span className='font-semibold text-slate-900'>{label}</span>
@@ -19,10 +23,12 @@ const TextArea = ({ name, label, rows, className }: IProps) => {
           'border border-slate-800 rounded-sm p-3 box-border',
           'focus:outline-none focus:border-pink-700 focus:ring focus:ring-pink-200',
           'hover:border-pink-700',
-          'transition-all duration-300',
+          'transition-colors duration-300',
           className,
         ])}
         rows={rows}
+        value={value}
+        onChange={onChange}
       ></textarea>
     </label>
   );
