3b320a9e103b056ed960a829e344dff9191e9fa2
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-22 17:39:30 +0100

    :truck: Added a Breadcrumb element

--- src/components/elements/Breadcrumb.tsx
+++ src/components/elements/Breadcrumb.tsx
@@ -1,2 +1,39 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
 
+interface BreadcrumbItem {
+  label: string;
+  href?: string;
+}
 
+interface IProps {
+  className?: string;
+
+  items: BreadcrumbItem[];
+}
+
+const Breadcrumb = ({ items, className }: IProps) => {
+  const getItem = ({ href, label }: BreadcrumbItem, index: number): ReactNode => {
+    if (index == 0) {
+      return (
+        <span key={index} className='text-slate-500'>
+          {label} &gt;{' '}
+        </span>
+      );
+    }
+
+    if (index == items.length - 1) {
+      return <span key={index}>{label}</span>;
+    }
+
+    return (
+      <a className='text-slate-500 hover:text-pink-700 transition-colors duration-300' key={index} href={href}>
+        {label} &gt;{' '}
+      </a>
+    );
+  };
+
+  return <p className={clsx(['text-sm font-medium', className])}>{items.map(getItem)}</p>;
+};
+
+export default Breadcrumb;
