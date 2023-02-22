fcdd2a7733a65d01b2284b5aaf3ef07451488c56
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-22 11:12:11 +0100

    :truck: Added a Table element

--- src/components/modules/Table.tsx
+++ src/components/modules/Table.tsx
@@ -8,9 +8,11 @@ interface IProps<T> {
   columns: { label: string; dataKey: keyof T; render?: (object: T[keyof T]) => ReactNode }[];
 
   data: T[];
+
+  onClick?: (row: T) => void;
 }
 
-const Table = <T,>({ columns, data, className }: IProps<T>) => {
+const Table = <T,>({ columns, data, onClick, className }: IProps<T>) => {
   return (
     <table className={clsx(['table-auto overflow-hidden rounded-sm border border-slate-200 border-separate border-spacing-0', className])}>
       <thead>
@@ -24,12 +26,12 @@ const Table = <T,>({ columns, data, className }: IProps<T>) => {
       </thead>
       <tbody>
         {data.map((row, index) => (
-          <tr key={index} className='group hover:bg-pink-200 transition-all duration-300 cursor-pointer'>
+          <tr key={index} className='group hover:bg-pink-200 transition-all duration-300 cursor-pointer' onClick={() => onClick && onClick(row)}>
             {columns.map(({ label, dataKey, render }, index) => (
               <td key={label + index} className='text-slate-700 group-hover:text-pink-800 transition-all duration-300 px-4 py-2'>
                 {render ? render(row[dataKey]) : (row[dataKey] as ReactNode)}
               </td>
-            ))}{' '}
+            ))}
           </tr>
         ))}
       </tbody>
