d332d8465b62dc3d56933961340bf11eedc4c11e
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-23 14:50:05 +0100

    :art: Made the menu active link noticeable

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -3,12 +3,15 @@ import { ReactNode, createRef, useEffect } from 'react';
 
 import menu from '@config/menu';
 import clsx from 'clsx';
+import { useRouter } from 'next/router';
 
 interface IProps {
   children?: ReactNode;
 }
 
 const Layout = ({ children }: IProps) => {
+  const router = useRouter();
+
   return (
     <div className='flex flex-row w-screen min-h-screen'>
       <nav className='h-screen text-slate-800 flex flex-col p-8 border-r border-slate-200 bg-gray-200'>
@@ -20,6 +23,7 @@ const Layout = ({ children }: IProps) => {
                 className={clsx([
                   'flex gap-2 items-center pl-4 pr-12 py-2 rounded',
                   'hover:bg-pink-400 hover:text-pink-800 hover:bg-opacity-50 transition-all duration-300',
+                  router.asPath.startsWith(href) && 'text-pink-800 font-medium',
                 ])}
               >
                 {icon}
