f7fed911d0f0f7cf54f2d77f40c40270975e5c67
ctrl-plus-w <lukas.ldrn@gmail.com>
2023-02-24 14:12:01 +0100

    :truck: Added an onClick method on the Tag element

--- src/components/elements/Tag.tsx
+++ src/components/elements/Tag.tsx
@@ -1,15 +1,18 @@
-import type { ReactNode } from 'react';
+import type { MouseEventHandler, ReactNode } from 'react';
 
 import clsx from 'clsx';
 
 interface IProps {
 	className?: string;
 	children?: ReactNode;
+
+	onClick?: MouseEventHandler<HTMLSpanElement>;
 }
 
-const Tag = ({ className, children }: IProps) => {
+const Tag = ({ className, children, onClick }: IProps) => {
 	return (
 		<span
+			onClick={onClick}
 			className={clsx([
 				'px-2 py-1 text-xs font-medium bg-pink-200 text-pink-800 rounded-sm border border-pink-300 ',
 				'group-hover:bg-pink-300 group-hover:text-pink-900 group-hover:border-pink-400 transition-all duration-300',
